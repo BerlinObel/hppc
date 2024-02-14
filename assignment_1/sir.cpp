4d44fc44e418f7084bc86470f7af14e4a6b04de9
BerlinObel <oscarobel@gmail.com>
2024-02-14 09:58:29 +0100

    add folder for organization

--- assignment_1/sir.cpp
+++ assignment_1/sir.cpp
@@ -16,15 +16,15 @@ void derivative(double S, double I,double& dS, double& dI, double& dR){
 int main(){
 	double I = 1;
 	double R = 0;
-	double S = (N - 1);
-	double step = 1;
+	double S = (N-1);
+	double step = 30;
 	double t = 0;
 	double dS, dI, dR;
 	std::ofstream myfile;
   	myfile.open ("sir_output.txt");
   	myfile << "t S I R \n";
 	myfile << t << " " << S << " " << I << " " << R << "\n";
-	while (t<200) {
+	while (t<200.0) {
 		t = t + step;
 		derivative(S,I,dS,dI,dR);
 		S += step*dS;
